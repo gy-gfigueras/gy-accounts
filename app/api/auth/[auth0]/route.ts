ff75578bf7104d827f7188beaaa389147e54f9c9
gy-gfigueras <willstar111yt@gmail.com>
2025-01-26 15:44:10 +0100

    fix: remove static generation

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,9 +1,3 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth();
-
-export async function generateStaticParams() {
-  return [
-    { auth0: "example" }, // Ajusta esto según tus necesidades
-  ];
-}
